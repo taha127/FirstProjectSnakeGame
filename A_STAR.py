b36ca6efbf7064af97adf81af8a359431ba7818c
taha127 <tahadehghani127@gmail.com>
2025-04-16 00:35:26 +0330

    Implement A* pathfinding algorithm in A_STAR.py

--- A_STAR.py
+++ A_STAR.py
@@ -10,14 +10,28 @@ class A_STAR(Algorithm):
         self.frontier = []
         self.explored_set = []
         self.path = []
-
         initialstate, goalstate = self.get_initstate_and_goalstate(snake)
-
-        # open list
+        initialstate.g = 0
+        initialstate.h = self.manhattan_distance(initialstate, goalstate)
+        initialstate.f = initialstate.g + initialstate.h
         self.frontier.append(initialstate)
-
-        # while we have states in open list
         while len(self.frontier) > 0:
-            # your code ----------------------------------------------------------------------
-
-            return None
+            current_node = min(self.frontier, key=lambda x: x.f)
+            self.frontier.remove(current_node)
+            if current_node.x == goalstate.x and current_node.y == goalstate.y:
+                return self.get_path(current_node)
+            self.explored_set.append(current_node)
+            for neighbor in self.get_neighbors(current_node):
+                if (neighbor in self.explored_set or
+                        self.inside_body(snake, neighbor) or
+                        self.outside_boundary(neighbor)):
+                    continue
+                tentative_g = current_node.g + 1
+                if neighbor not in self.frontier or tentative_g < neighbor.g:
+                    neighbor.parent = current_node
+                    neighbor.g = tentative_g
+                    neighbor.h = self.manhattan_distance(neighbor, goalstate)
+                    neighbor.f = neighbor.g + neighbor.h
+                    if neighbor not in self.frontier:
+                        self.frontier.append(neighbor)
+        return None
