79577c579f3c101af371c05f7acbdb155aee7d76
taha127 <tahadehghani127@gmail.com>
2025-04-16 00:17:02 +0330

    Implement recursive DFS algorithm for pathfinding in DFS.py

--- DFS.py
+++ DFS.py
@@ -7,29 +7,42 @@ class DFS(Algorithm):
         super().__init__(grid)
 
     def recursive_DFS(self, snake, goalstate, currentstate):
-        # your code ----------------------------------------------------------------------
+        if (currentstate.x, currentstate.y) == (goalstate.x, goalstate.y):
+            self.path.append(currentstate)
+            return currentstate
 
+        self.explored_set.add((currentstate.x, currentstate.y))
+
+        for neighbor in self.get_neighbors(currentstate):
+            if (neighbor.x, neighbor.y) not in self.explored_set and \
+                    not self.outside_boundary(neighbor) and \
+                    not self.inside_body(snake, neighbor):
+                next_step = self.recursive_DFS(snake, goalstate, neighbor)
+                if next_step:
+                    if currentstate != self.initial_state:
+                        self.path.append(currentstate)
+                        return currentstate
+                    self.path.pop()
+                    return next_step
         return None
 
     def run_algorithm(self, snake):
         # to avoid looping in the same location
         if len(self.path) != 0:
             # while you have path keep going
-            path = self.path.pop()
+            next_step = self.path.pop()
 
-            if self.inside_body(snake, path):
-                self.path = [] # or calculate new path!
+            if self.inside_body(snake, next_step):
+                self.path = []  # or calculate new path!
             else:
-                return path
+                return next_step
 
-        # start clean
+            # start clean
         self.frontier = []
-        self.explored_set = []
+        self.explored_set = set()
         self.path = []
-
-        initialstate, goalstate = self.get_initstate_and_goalstate(snake)
-
-        self.frontier.append(initialstate)
+        self.initial_state, goal_state = self.get_initstate_and_goalstate(snake)
 
         # return path
-        return self.recursive_DFS(snake, goalstate, initialstate)
+        ns = self.recursive_DFS(snake, goal_state, self.initial_state)
+        return ns
